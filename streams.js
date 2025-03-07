803711bf1e32f4588a3be75b53f1671b3f995f73
Sannu Sharma <sannusharma02@gmail.com>
2025-03-07 09:38:34 +0530

    loading with Readable Stream and check error

--- streams.js
+++ streams.js
@@ -2,10 +2,24 @@ const fs = require('fs');
 const server = require('http').createServer();
 
 server.on('request', (req, res) => {
-    fs.readFile('test-file.txt', 'utf8', (err, data) => {
-        if (err) console.log(err);
-        res.end(data);
+
+    // fs.readFile('test-file.txt', 'utf8', (err, data) => {
+    //     if (err) console.log(err);
+    //     res.end(data);
+    // });
+
+    const readable = fs.createReadStream('test-file.txt');
+    readable.on('data', (chunk) => {
+       res.write(chunk);
     });
+    readable.on('end', () => {
+        res.end();
+    })
+    readable.on('error', (err) => {
+        console.error(err);
+        res.statusCode = 500;
+        res.end("File not found!");
+    })
 });
 
 server.listen(8000,'localhost', (err) => {
