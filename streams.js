f283d39a5ba22f524bfc6991c3715d60f8363080
Sannu Sharma <sannusharma02@gmail.com>
2025-03-07 10:07:10 +0530

    loading with Readable Stream with proper way to not create any read write issue

--- streams.js
+++ streams.js
@@ -2,24 +2,29 @@ const fs = require('fs');
 const server = require('http').createServer();
 
 server.on('request', (req, res) => {
-
+// Sol1
     // fs.readFile('test-file.txt', 'utf8', (err, data) => {
     //     if (err) console.log(err);
     //     res.end(data);
     // });
-
+// Sol2
+    // const readable = fs.createReadStream('test-file.txt');
+    // readable.on('data', (chunk) => {
+    //    res.write(chunk);
+    // });
+    // readable.on('end', () => {
+    //     res.end();
+    // });
+    // readable.on('error', (err) => {
+    //     console.error(err);
+    //     res.statusCode = 500;
+    //     res.end("File not found!");
+    // });
+//Sol3
     const readable = fs.createReadStream('test-file.txt');
-    readable.on('data', (chunk) => {
-       res.write(chunk);
-    });
-    readable.on('end', () => {
-        res.end();
-    })
-    readable.on('error', (err) => {
-        console.error(err);
-        res.statusCode = 500;
-        res.end("File not found!");
-    })
+    readable.pipe(res);
+    //readableSource.pipe(writeableDest)
+
 });
 
 server.listen(8000,'localhost', (err) => {
